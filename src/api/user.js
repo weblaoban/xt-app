71ce316e18b406e6f9aba766e48dd23ad371bd60
馒头煤球 <13737914+mantou-briquette@user.noreply.gitee.com>
2023-11-30 22:32:08 +0800

    fix:apis

--- src/api/user.js
+++ src/api/user.js
@@ -2,80 +2,89 @@ import request from "@/axios";
 import { baseUrl } from "@/config/env";
 // 登录
 export const loginByUsername = (data) =>
-	request({
-		url: baseUrl + "/login",
-		method: "post",
-		meta: {
-			isToken: false,
-		},
-		data: {
-			...data,
-		},
-	});
+    request({
+        url: baseUrl + "/login",
+        method: "post",
+        meta: {
+            isToken: false,
+        },
+        data: {
+            ...data,
+        },
+    });
 
 export const refreshToken = () =>
-	request({
-		url: baseUrl + "/user/refresh",
-		method: "post",
-	});
+    request({
+        url: baseUrl + "/user/refresh",
+        method: "post",
+    });
 
 export const getMenu = (type = 0) =>
-	request({
-		url: baseUrl + "/user/getMenu",
-		method: "get",
-		params: {
-			type,
-		},
-	});
+    request({
+        url: baseUrl + "/user/getMenu",
+        method: "get",
+        params: {
+            type,
+        },
+    });
 
 export const getTopMenu = () =>
-	request({
-		url: baseUrl + "/user/getTopMenu",
-		method: "get",
-	});
+    request({
+        url: baseUrl + "/user/getTopMenu",
+        method: "get",
+    });
 
 export const sendLogs = (list) =>
-	request({
-		url: baseUrl + "/user/logout",
-		method: "post",
-		data: list,
-	});
+    request({
+        url: baseUrl + "/user/logout",
+        method: "post",
+        data: list,
+    });
 
 export const logout = () =>
-	request({
-		url: baseUrl + "/user/logout",
-		meta: {
-			isToken: false,
-		},
-		method: "get",
-	});
+    request({
+        url: baseUrl + "/user/logout",
+        meta: {
+            isToken: false,
+        },
+        method: "get",
+    });
 
 // 注册
 export const register = (data) =>
-	request({
-		url: baseUrl + "/user/register",
-		method: "post",
-		data: data,
-	});
+    request({
+        url: baseUrl + "/user/register",
+        method: "post",
+        data: data,
+    });
 
 // 获取用户信息
 export const getUserInfo = () =>
-	request({
-		url: baseUrl + "/p/user/userInfo",
-		method: "get",
-	});
+    request({
+        url: baseUrl + "/p/user/userInfo",
+        method: "get",
+    });
 // 修改密码
 export const modifyPassword = (data) =>
-	request({
-		url: baseUrl + "/user/updatePwd",
-		method: "put",
-		data: data,
-	});
+    request({
+        url: baseUrl + "/user/updatePwd",
+        method: "put",
+        data: data,
+    });
 
 // 发送短信验证码
 export const sendSmsCode = (data) =>
-	request({
-		url: baseUrl + "/p/sms/send",
-		method: "post",
-		data: data,
-	});
+    request({
+        url: baseUrl + "/p/sms/send",
+        method: "post",
+        data: data,
+    });
+
+// 用户购买详情 参数 uid
+export const getUserProd = (params) => {
+    return request({
+        url: baseUrl + '/admin/prodTagReference/find',
+        method: 'get',
+        params: { ...params }
+    })
+}
